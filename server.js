2a80a129272ae41507fe492a3b6b4281ae6b2546
gmc314 <grant.cheng1@gmail.com>
2024-04-11 14:16:07 -0400

    format fixes

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-import express from "express";
-import cors from "cors";
-import knex from "knex";
-import bcrypt from "bcrypt";
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex");
 
-import { handleRegister } from "./controllers/register";
-import { handleSignin } from "./controllers/signin";
-import { handleProfile } from "./controllers/profile";
-import { handleImage } from "./controllers/image";
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const postgres = knex({
   client: "pg"
 });
 
 const app = express();
-app.use(json());
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {res.send("success")});
-app.post("/signin", (req, res) => {handleSignin(req, res, postgres, bcrypt)});
-app.post("/register", (req, res) => {handleRegister(req, res, postgres, bcrypt)});
-app.get("/profile/:id", (req, res) => {handleProfile(req, res, postgres)});
-app.put("/image", (req, res) => {handleImage(req, res, postgres)});
+app.post("/signin", (req, res) => {signin.handleSignin(req, res, postgres, bcrypt)});
+app.post("/register", (req, res) => {register.handleRegister(req, res, postgres, bcrypt)});
+app.get("/profile/:id", (req, res) => {profile.handleProfile(req, res, postgres)});
+app.put("/image", (req, res) => {image.handleImage(req, res, postgres)});
 
 app.listen(3000, () => {console.log("app running on 3000")});
