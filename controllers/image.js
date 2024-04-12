cd38955a7b2d15e91172c3e0263f73e47170e1ac
gmc314 <grant.cheng1@gmail.com>
2024-04-12 13:53:07 -0400

    debug

--- controllers/image.js
+++ controllers/image.js
@@ -4,7 +4,7 @@ const handleImage = (req, res, postgres) => {
   .increment("entries", 1)
   .returning("entries")
   .then(entries => {
-    res.json(entries[0])
+    res.json(entries[0].entries)
   })
   .catch(error => res.status(400).json("entries not found"))
 };
