6df3e1056bde2f053d1dc3cc42a377450c1dba89
gmc314 <grant.cheng1@gmail.com>
2024-08-13 13:32:26 -0400

    update axois

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const bcrypt = require("bcrypt");
 const cors = require("cors");
 const knex = require("knex");
 
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
+const register = require("./controllers/register.js");
+const signin = require("./controllers/signin.js");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image.js");
 
 const postgres = knex({
   client: "pg",
